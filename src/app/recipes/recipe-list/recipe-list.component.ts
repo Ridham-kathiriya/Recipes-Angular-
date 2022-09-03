683b90d76082bd8fc9f36aba6cadf37fa39e0d61
Ridham-kathiriya <ridham.kathiriya@dal.ca>
2022-09-03 09:55:46 -0300

    Recipes Initials

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'A test only',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+    new Recipe(
+      'Another test Recipe',
+      'Another test only',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
